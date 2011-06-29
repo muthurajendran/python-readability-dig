ad38fac40a4043c71144fef3c8c2376474b0700a
Jerry Charumilind <git@jcharum.fastmail.net>
2011-06-30 05:00:30 +0800

    Add chardet to installation requirements

--- setup.py
+++ setup.py
@@ -11,10 +11,13 @@ setup(
     license="Apache License 2.0",
     url="http://github.com/buriy/python-readability",
     packages=find_packages(),
+    install_requires=[
+        "chardet"
+        ],
     classifiers=[
         "Environment :: Web Environment",
         "Intended Audience :: Developers",
         "Operating System :: OS Independent",
         "Programming Language :: Python",
-    ],
+        ],
 )
