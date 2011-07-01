f189ab905daa616d502617290ef472a801e3b926
Yuri Baburov <burchik@gmail.com>
2011-07-02 00:24:15 +0700

    Fixed README for pypi.

--- setup.py
+++ setup.py
@@ -3,10 +3,10 @@ from setuptools import setup, find_packages
 
 setup(
     name="readability-lxml",
-    version="0.2",
+    version="0.2.1",
     author="Yuri Baburov",
     author_email="burchik@gmail.com",
-    description="python port of arc90's readability bookmarklet",
+    description="fast python port of arc90's readability tool",
     long_description=open("README").read(),
     license="Apache License 2.0",
     url="http://github.com/buriy/python-readability",
