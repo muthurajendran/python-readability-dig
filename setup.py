1a4d3697bcca0117fbe8849d600966532f813f3a
Yuri Baburov <burchik@gmail.com>
2014-04-02 15:16:19 +0700

    Allow latest lxml on Mac OS X 10.9, see issue #39 for comments and setup instructions

--- setup.py
+++ setup.py
@@ -2,14 +2,17 @@
 from setuptools import setup, find_packages
 import sys
 
+lxml_requirement = "lxml"
 if sys.platform == 'darwin':
-    lxml = "lxml<2.4"
-else:
-    lxml = "lxml"
+    import platform
+    mac_ver = platform.mac_ver()[0]
+    if mac_ver < '10.9':
+        print "Using lxml<2.4"
+        lxml_requirement = "lxml<2.4"
 
 setup(
     name="readability-lxml",
-    version="0.3.0.2",
+    version="0.3.0.3",
     author="Yuri Baburov",
     author_email="burchik@gmail.com",
     description="fast python port of arc90's readability tool",
@@ -20,7 +23,7 @@ setup(
     packages=['readability'],
     install_requires=[
         "chardet",
-        lxml
+        lxml_requirement
         ],
     classifiers=[
         "Environment :: Web Environment",
